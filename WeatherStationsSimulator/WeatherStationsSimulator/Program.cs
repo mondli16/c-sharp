d832ff7666ff56bb3603347040842604ec9dc5c5
mondli16 <125990471+mondli16@users.noreply.github.com>
2024-12-10 20:29:14 +0200

    Add files via upload

---
+++ WeatherStationsSimulator/WeatherStationsSimulator/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Security;
+
+namespace WeatherStationsSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days to simulate:");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature= new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] watherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                 watherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+            Console.WriteLine($"The max temp is {temperature.Max()}");
+            Console.WriteLine($"The min temp is {temperature.Min()}");
+            Console.WriteLine($"The min temp is {MinTemp(temperature)}");
+            Console.WriteLine($"The avarage temparature is {CalculateAverage(temperature)}");
+            Console.WriteLine($"Most Common {MostCommonCondition(conditions)}");
+
+        }
+        static double CalculateAverage(int[] temperature)
+        {
+            int sum = 0;
+            for (int i = 0; i < temperature.Length; i++)
+            {
+                sum += temperature[i];
+            }
+
+            double average = sum / temperature.Length;
+            return average;
+        }
+
+        static int MinTemp(int[] temperature)
+        { 
+            int min = temperature[0];
+
+            foreach (int i in temperature) 
+            { 
+            if (i < min)
+                {
+                    min= i;
+                }
+            }   
+            return min;
+        }
+
+        static string MostCommonCondition(string[] condition)
+        {
+            int count = 0;
+            string mostCommon = condition[0];
+
+            for (int i = 0; i < condition.Length; i++)
+            {
+                int tempCount = 0;
+                for (int j = 0; j < condition.Length; j++)
+                {
+                    if (condition[j] == condition[i])
+                    {
+                        tempCount++;
+                    }
+                    if(tempCount > count)
+                    {
+                        count = tempCount;
+                        mostCommon = condition[i];
+
+                    }
+                    }
+
+            }
+            return mostCommon;
+        }
+        
+    }
+}
---
+++ threadsApp/threadsApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http.Headers;
+
+namespace threadsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World! 1");
+            //Thread.Sleep(1000);
+
+            //Console.WriteLine("Hello, World! 2");
+            //Thread.Sleep(1000);
+
+            //Console.WriteLine("Hello, World! 3");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello, World! 4");
+            //Thread.Sleep(1000);
+
+            //Console.WriteLine("Hello, World! 5");
+            //Thread.Sleep(1000);
+
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("TThread 1");
+            //}).Start();
+
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("TThread 2");
+            //}).Start();
+
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("TThread 3");
+            //}).Start();
+
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("TThread 4");
+            //}).Start();
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() => 
+            { 
+                Thread.Sleep(1000);
+                taskCompletionSource.TrySetResult(true);
+            });
+            Console.WriteLine(thread.ManagedThreadId);
+            thread.Start();
+            var test = taskCompletionSource.Task.Result;
+
+            
+
+         
+        }
+    }
+}
