1da345defaa879adc4d250eaa85bae944a6cc777
mondli16 <125990471+mondli16@users.noreply.github.com>
2024-12-10 20:20:19 +0200

    Add files via upload

---
+++ ManagementApp/ManagementApp/LoginView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManagementApp
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        public void OnLoginBtnClicked(object sender, EventArgs e)
+        {
+            string passwordEntered = PasswordBox.Password;
+
+            string? envPw = Environment.GetEnvironmentVariable("InvoiceManagement")?.ToString();
+
+
+            if (passwordEntered != null)
+            {
+
+              if (!string.IsNullOrEmpty(envPw))
+                {
+                    MessageBox.Show("Correct Password");
+                }
+              else
+                {
+                    MessageBox.Show("Incorrect Password");
+                };
+            }
+            else
+            {
+                MessageBox.Show("Environment variable not found");
+            }
+
+    
+        }
+
+        public void OnPasswordChange(object sender, EventArgs e)
+        { 
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(PasswordBox.Password);
+        }
+    }
+
+}
